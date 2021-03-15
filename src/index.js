b7563d2e1af321e9210e3fa2a8718775d5d7801c
Dzmitry <74104111+GiperSi@users.noreply.github.com>
2021-03-15 21:08:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let object = {};
+  for (PairedBrackets of bracketsConfig) { object[PairedBrackets[1]] = PairedBrackets[0]};
+  let BrClose = Object.keys(object);
+  return str.split('').reduce((stack, brace) => {
+    BrClose.includes(brace) && stack[stack.length - 1] === object[brace] ? stack.pop() : stack.push(brace);
+    return stack;
+  }, []).length === 0;
 }
